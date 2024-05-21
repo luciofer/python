1f266de58262b9b96c53ee7da3dafeb65614a0ea
Lucio Araujo <contatolucioaraujo@gmail.com>
2024-05-21 10:18:29 -0300

    Add lambdas

---
+++ lambdas.py
@@ -0,0 +1,21 @@
+import math
+
+#Basic
+calc = lambda x, y: x + y
+
+authors = ["William Shakespeare", "Johann Wolfgang von Goethe", "Miguel de Cervantes", "J.K. Rowling"]
+
+authors.sort(key= lambda surname: surname.split(" ")[-1].lower())
+
+print(authors)
+
+print("------------------------------------------")
+#Map - Returns a map object
+
+radius = [1, 2, 3, 4, 5]
+areas = map(lambda r: math.pi * (r ** 2), radius)
+
+print(list(areas))
+print(list(map(lambda r:  math.pi * (r ** 2), radius)))
+
+print("------------------------------------------")
